63135c881ae758251694cc8b317ad54fb6a58a59
Alexandr Poltavsky <alexpolt@yandex.ru>
2016-04-06 06:47:09 +0300

    at last it's ready

--- ps2.hlsl
+++ ps2.hlsl
@@ -16,13 +16,17 @@ float4 main( float2 screenxy : TEXCOORD0 ) : COLOR0 {
   float4 color0 = tex2D( tex0, screenxy );
   float2 dfaa = tex2D( tex1, screenxy ).xy;
   float area = dfaa.y;
-  //decode direction, angle of 0 is valid, so we need this area > 0 check
-  float2 dir = area > 0 ? float2( cos( dfaa.x * pi2 ), sin( dfaa.x * pi2 ) ) : 0;
+  //decode direction
+  float2 dir = float2( cos( dfaa.x * pi2 ), sin( dfaa.x * pi2 ) );
   float2 sdx = ddx( screenxy );
   float2 sdy = ddy( screenxy );
-
-  float4 color1 = tex2D(tex0, screenxy + sdx*dir.x + sdy*dir.y );
-
+  
+  float4 color1 = color0;
+  
+  //only do fetch if necessary (edge pixels)
+  if( area > 0 && area < 1 ) 
+    color1 = tex2D(tex0, screenxy + sdx*dir.x + sdy*dir.y );
+  
   float4 color = lerp( color1, color0, area );
 
   return float4( color.rgb, 1 );
