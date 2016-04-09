355d38f7309cb4c8c3365c68de6207cb76c9282e
Alexandr Poltavsky <alexpolt@yandex.ru>
2016-04-09 22:38:23 +0300

    added noperspective to uvs

--- ps1.hlsl
+++ ps1.hlsl
@@ -11,7 +11,7 @@ float DFAA1( float2 uv01 );
 static const float pi2 = 2*3.1415926;
 
 /* first pass pixel shader */
-float4 main( float2 uv01: TEXCOORD0 ) : COLOR0
+float4 main( noperspective float2 uv01: TEXCOORD0 ) : COLOR0
 {   
 	float dfaa = DFAA1( uv01 );
 
